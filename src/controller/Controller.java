5191045194cd989c4639f58ab4a7c612deffbd62
thatsmellything <thesmelliestman@gmail.com>
2021-02-25 01:09:22 -0700

    port scanner almost works. Errors in thread making

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,10 @@
 package controller;
 
 import navigation.*;
+import readyForImplementation.PortScanner;
+
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import apps.*;
@@ -29,12 +32,48 @@ public class Controller {
 		Scanner inputScanner = new Scanner(System.in);
 		String input = inputScanner.nextLine();
 		int x = Integer.parseInt(input);
+		
+	//Now it is selecting the apps	
+		//public ip lookup
 		if( x == 1)
 		{
 			try {
 				PublicIPLookupHost.main(null);
 				restart();
 			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				restart();
+			}
+		}
+		//Port Scanner
+		if(x ==2) {
+			System.out.println("Input the IP you wish to scan (deafult is local host 127.0.0.1): ");
+			String ip = inputScanner.nextLine();
+			System.out.println("Input number of threads for port scan (default is 0 which is the fastest): ");
+			String threads = inputScanner.nextLine();
+			if(threads.equals(""))
+			{
+				threads = "0";
+			}
+			
+			if(ip.equals("")) {
+				try {
+					System.out.println("Scanning local host 127.0.0.1 with " + threads+ " threads");
+					PortScanner.localPortScan(threads);
+					restart();
+				} catch (UnknownHostException | InterruptedException | ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					restart();
+				}
+			}
+			
+			try {
+				System.out.println("Scanning "+ip+" with " + threads+ " threads");
+				PortScanner.foreignPortScan(ip, threads);
+				restart();
+			} catch (UnknownHostException | InterruptedException | ExecutionException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				restart();
