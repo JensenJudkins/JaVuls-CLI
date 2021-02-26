0eb450860d6ea5277e462da005479fca3cef7631
thatsmellything <thesmelliestman@gmail.com>
2021-02-25 19:46:49 -0700

    Update MainMenu.java

--- src/navigation/MainMenu.java
+++ src/navigation/MainMenu.java
@@ -13,7 +13,7 @@ public class MainMenu {
     public static void mainMenuList(){
     	System.out.println("");
     	System.out.println("1) List (your) Public IP");
-    	
+    	System.out.println("2) Port Scanner");
     }
     
 }
