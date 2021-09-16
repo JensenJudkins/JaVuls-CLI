c63ffb277ba79abc7520271ca493f06dbc5a852d
thatsmellything <thesmelliestman@gmail.com>
2021-09-16 08:22:58 -0600

    Should work now given an interface actually exists

---
+++ src/testApplications/MonitorModeScript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Going to put device into monitor mode on $1 (linux only)"
+echo "installing dependicies (aircrack-ng suite)"
+sudo apt install aircrack-ng -y
+echo "Setting $1 to monitor mode"
+echo "Killing processes"
+sudo airmon-ng --check-kill 
+echo "Setting up"
+sudo airmon-ng start $1
--- src/testApplications/StartMonitorMode.java
+++ src/testApplications/StartMonitorMode.java
@@ -1,14 +1,35 @@
 package testApplications;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class StartMonitorMode {
+    public static String pathToScript = new File("MonitorModeScript.sh").getAbsolutePath();
  public static void main(String[] args) {
-  Process p;
+    Scanner inputScanner = new Scanner(System.in);
+    System.out.println("Please input the interface to set to monitor mode");
+    //String input = inputScanner.nextLine();
+    String interfaceToChange = inputScanner.nextLine();
+    System.out.println(new File("MonitorModeScript.sh").getAbsolutePath());
+    System.out.println(pathToScript);
+    if(interfaceToChange.equals(""))
+      {
+          interfaceToChange = "wlan0";
+      }
+     StartMonMode(interfaceToChange);
+ }
+ public static void StartMonMode(String interfaceToChange)
+ {
+ Process p;
   try {
-   String[] cmd = { "sh", "MonitorModeScript.sh" + " " + args[0]};
+      if(interfaceToChange.equals(""))
+      {
+          interfaceToChange = "wlan0";
+      }
+   String[] cmd = { "sh", pathToScript, interfaceToChange};
    p = Runtime.getRuntime().exec(cmd); 
    p.waitFor(); 
    BufferedReader reader=new BufferedReader(new InputStreamReader(
