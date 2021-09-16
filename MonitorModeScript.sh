ef7ae63c6bd2cbb992cab162eac5548d0cc84565
Jensen Judkins <thesmelliestman@gmail.com>
2021-09-16 12:44:55 -0600

    Update MonitorModeScript.sh

--- MonitorModeScript.sh
+++ MonitorModeScript.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Updating"
+sudo apt update
 echo "Going to put device into monitor mode on $1 (linux only)"
 echo "installing dependicies (aircrack-ng suite)"
 sudo apt install aircrack-ng -y
