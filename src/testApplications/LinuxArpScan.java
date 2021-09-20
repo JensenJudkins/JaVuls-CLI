9886ec0e5ec456224849b21a600644a5c5ad40cc
thatsmellything <thesmelliestman@gmail.com>
2021-09-20 09:46:29 -0600

    Added installer for arp scan on linux. Just waiting to get ip subnet figured out

--- src/testApplications/LinuxArpScan.java
+++ src/testApplications/LinuxArpScan.java
@@ -3,6 +3,7 @@ package testApplications;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import apps.Enumeration.LocalIPLookupHost;
 import testApplications.ShowProperties;
 
 public class LinuxArpScan {
@@ -14,27 +15,50 @@ public class LinuxArpScan {
 		}
 		else
 		{
-			 String s;
-		        Process p;
-		        try {
-		            p = Runtime.getRuntime().exec("ls -aF");
-		            BufferedReader br = new BufferedReader(
-		                new InputStreamReader(p.getInputStream()));
-		            while ((s = br.readLine()) != null)
-		                System.out.println("line: " + s);
-		            p.waitFor();
-		            System.out.println ("exit: " + p.exitValue());
-		            p.destroy();
-		        } catch (Exception e) {}
+			String s;
+			Process p;
+			try {
+				p = Runtime.getRuntime().exec("sudo apt-get install arp-scan");
+				BufferedReader br = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+				while ((s = br.readLine()) != null)
+					System.out.println("line: " + s);
+				p.waitFor();
+				System.out.println ("exit: " + p.exitValue());
+				p.destroy();
+			} catch (Exception e) {}
+			try {
+				p = Runtime.getRuntime().exec("sudo arp-scan --interface=eth0 " +  LocalIPLookupHost.getLocalIP() +"/24");
+				BufferedReader br = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+				while ((s = br.readLine()) != null)
+					System.out.println("line: " + s);
+				p.waitFor();
+				System.out.println ("exit: " + p.exitValue());
+				p.destroy();
+			} catch (Exception e) {}
 		}
     }
 	
 	public void arpScanLinux()
 	{
+		/*
 		String s;
         Process p;
         try {
             p = Runtime.getRuntime().exec("ls -aF");
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(p.getInputStream()));
+            while ((s = br.readLine()) != null)
+                System.out.println("line: " + s);
+            p.waitFor();
+            System.out.println ("exit: " + p.exitValue());
+            p.destroy();
+			*/
+			String s;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("sudo apt-get install arp-scan");
             BufferedReader br = new BufferedReader(
                 new InputStreamReader(p.getInputStream()));
             while ((s = br.readLine()) != null)
